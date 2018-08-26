7d5c110b0479d00db00421f2fdd24e0b4c75a2a4
Aliaksandr Aranski <aranich@ya.ru>
2018-08-26 11:09:22 +0300

    Powershell script to zip in Windows

---
+++ CustomWebsite.ps1
@@ -0,0 +1,86 @@
+Configuration CustomWebsite
+{
+    Import-DscResource -Module xWebAdministration    
+
+    param ($MachineName)
+
+    Node $MachineName
+    {
+        File DemoFile
+        {
+            DestinationPath        = 'C:\CustomWebsite\index.html'
+	    Ensure                 = 'Present'
+	    Type                   = 'File'
+	    Contents               = "<h2 style='color:$Node.TextColor;'>The page from $($MachineName)</h2>"
+	    Force                  = $true
+	}
+
+        WindowsFeature IIS
+        {
+            Ensure                 = 'Present'
+            Name                   = 'Web-Server'
+        {
+
+	WindowsFeature IISManagement
+	{
+	    Ensure                 = 'Present'
+	    Name                   = 'Web-Mgmt-Tools'
+	}
+
+	WindowsFeature AspNet45
+	{
+	    Ensure                 = 'Present'
+	    Name                   = 'Web-Asp-Net45'
+	}
+
+	xWebsite DefaultSite
+	{
+	    Ensure                 = 'Present'
+	    Name                   = 'Default Web Site'
+	    State                  = 'Stopped'
+	    PhysicalPath           = $Node.DefaultWebSitePath
+	    DependsOn              = "[WindowsFeature]IIS"
+	}
+
+	File WebContent
+	{
+	    Ensure                 = 'Present'
+	    SourcePath             = $Node.SourcePath
+	    Destination            = $Node.DestinationPath
+	    Recurse                = $true
+            Type                   = "Directory"
+	    DependsOn              = "[WindowsFeature]AspNet45"
+	}
+
+	xWebsite CustomWebSite
+	{
+	    Ensure                 = 'Present'
+	    Name                   = $Node.WebsiteName
+	    State                  = 'Started'
+	    PhysicalPath           = $Node.DestinationPath
+	    DependsOn              = "[File]WebContent"
+	}
+    }
+}
+
+$ConfigurationData = @{
+	AllNodes = @(
+	    @{
+	        NodeName           = "*"
+		WebsiteName        = "AzureTest"
+		SourcePath         = "C:\CustomWebsite\"
+		DestinationPath    = "C:\inetpub\AzureTest"
+		DefaultWebsitePath = "C:\inetpub\wwwroot"
+	    },
+	    @{
+		NodeName           = "server1.azuretest.com"
+		Role               = "Web"
+		TextColor          = "green"
+	    },
+	    @{
+		NodeName           = "server2.azuretest.com"
+		Role               = "Web"
+		TextColor          = "red"
+	    }
+	)
+}
