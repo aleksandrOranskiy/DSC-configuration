9d2c52022e5d8ddfa1459869f8da6e79fe16f1b4
Aliaksandr Aranski <aranich@ya.ru>
2018-08-26 20:45:46 +0300

    Deleted all whitespaces

--- CustomWebsite.ps1
+++ CustomWebsite.ps1
@@ -1,67 +1,68 @@
 Configuration CustomWebsite
 {
 
-    Import-DscResource -ModuleName PSDesiredStateConfiguration    
+Import-DscResource -ModuleName PSDesiredStateConfiguration    
+param ($MachineName)
 
-    param ($MachineName)
+Node $MachineName
+{
 
-    Node $MachineName
-    {
-        File DemoFile
-        {
-            DestinationPath        = 'C:\CustomWebsite\index.html'
-	    Ensure                 = 'Present'
-	    Type                   = 'File'
-	    Contents               = "<h2 style='color:$Node.TextColor;'>The page from $($MachineName)</h2>"
-	    Force                  = $true
-	}
+File DemoFile
+{
+DestinationPath = 'C:\CustomWebsite\index.html'
+Ensure = 'Present'
+Type = 'File'
+Contents = "<h2 style='color:$Node.TextColor;'>The page from $($MachineName)</h2>"
+Force = $true
+}
 
-        WindowsFeature IIS
-        {
-            Ensure                 = 'Present'
-            Name                   = 'Web-Server'
-        {
+WindowsFeature IIS
+{
+Ensure = 'Present'
+Name = 'Web-Server'
+}
 
-	WindowsFeature IISManagement
-	{
-	    Ensure                 = 'Present'
-	    Name                   = 'Web-Mgmt-Tools'
-	}
+WindowsFeature IISManagement
+{
+Ensure = 'Present'
+Name = 'Web-Mgmt-Tools'
+}
 
-	WindowsFeature AspNet45
-	{
-	    Ensure                 = 'Present'
-	    Name                   = 'Web-Asp-Net45'
-	}
+WindowsFeature AspNet45
+{
+Ensure = 'Present'
+Name = 'Web-Asp-Net45'
+}
 
-	File WebContent
-	{
-	    Ensure                 = 'Present'
-	    SourcePath             = $Node.SourcePath
-	    DestinationPath        = $Node.DefaultWebsitePath
-	    DependsOn              = "[WindowsFeature]AspNet45"
-	}
-    }
+File WebContent
+{
+Ensure = 'Present'
+SourcePath = $Node.SourcePath
+DestinationPath = $Node.DefaultWebsitePath
+DependsOn = "[WindowsFeature]AspNet45"
+}
+
+}
 }
 
 $ConfigurationData = @{
-	AllNodes = @(
-	    @{
-	        NodeName           = "*"
-		WebsiteName        = "AzureTest"
-		SourcePath         = "C:\CustomWebsite\index.html"
-		DestinationPath    = "C:\inetpub\AzureTest"
-		DefaultWebsitePath = "C:\inetpub\wwwroot"
-	    },
-	    @{
-		NodeName           = "server1.azuretest.com"
-		Role               = "Web"
-		TextColor          = "green"
-	    },
-	    @{
-		NodeName           = "server2.azuretest.com"
-		Role               = "Web"
-		TextColor          = "red"
-	    }
-	)
+AllNodes = @(
+@{
+NodeName = "*"
+WebsiteName = "AzureTest"
+SourcePath = "C:\CustomWebsite\index.html"
+DestinationPath = "C:\inetpub\AzureTest"
+DefaultWebsitePath = "C:\inetpub\wwwroot"
+},
+@{
+NodeName = "server1.azuretest.com"
+Role = "Web"
+TextColor = "green"
+},
+@{
+NodeName = "server2.azuretest.com"
+Role = "Web"
+TextColor = "red"
+}
+)
 }
